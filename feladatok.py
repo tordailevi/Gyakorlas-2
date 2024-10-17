73722985de06b07f5851a6e58c40d76a9d72be26
Torderius <tordailevi@gmail.com>
2024-10-17 11:17:54 +0200

    Minden szuper

--- feladatok.py
+++ feladatok.py
@@ -1,3 +1,4 @@
+import random
 '''Adott egy lista
 lista= [12, 21, 56, 32, -5, -23, 35]'''
 '''Paraméterként a fenti listát!'''
@@ -45,3 +46,95 @@ def otteloszthato(lista):
 
     return atlag
     
+'''Függvény: éremdobás-10db fej vagy írás, eredményt eltárolja egy listában
+A függvény térjen vissza a listával.'''
+
+def eremdobas():
+    eredmenyek_lista=[]
+    i:int=0
+    while(i <10):
+        erem:int=int(random.random() * 2) +1
+        if(erem==1):
+            eredmenyek_lista.append("fej")
+        elif(erem==2):
+            eredmenyek_lista.append("iras")
+        i += 1
+    return eredmenyek_lista
+
+def fej_dobasok(eredmenyek_lista):
+    i:int=0
+    fej:int=0
+    
+
+    while (i < len(eredmenyek_lista)):
+        
+        if (eredmenyek_lista[i] == "fej"):
+            fej += 1
+        i += 1
+    return fej
+
+def kockadobas():
+    kockadobasok=[]
+    i=0
+    while(i<200):
+        dobas:int=int(random.random()*7)+1
+        if(dobas ==1):
+            kockadobasok.append(dobas)
+            i += 1
+        elif(dobas ==2):
+            kockadobasok.append(dobas)
+            i += 1
+        elif(dobas ==3):
+            kockadobasok.append(dobas)
+            i += 1
+        elif(dobas ==4):
+            kockadobasok.append(dobas)
+            i += 1
+        elif(dobas ==5):
+            kockadobasok.append(dobas)
+            i += 1
+        elif(dobas ==6):
+            kockadobasok.append(dobas)
+            i += 1
+    return kockadobasok, dobas
+
+
+def kockadobas_szamolo(kockadobasok, dobas):
+    i:int=0
+    egyes:int=0
+    kettes:int=0
+    harmas:int=0
+    negyes:int=0
+    otos:int=0
+    hatos:int=0
+    while (i < len(kockadobasok)):
+        if (dobas == 1):
+            egyes += 1
+            i += 1
+
+        elif (dobas == 2):
+            kettes += 1
+            i += 1
+
+        elif (dobas == 3):
+            harmas += 1
+            i += 1
+
+        elif (dobas == 4):
+            negyes += 1
+            i += 1
+
+        elif (dobas == 5):
+            otos += 1
+            i += 1
+
+        elif (dobas == 6):
+            hatos += 1
+            i += 1
+    i += 1
+    return egyes, kettes, harmas, negyes, otos, hatos
+
+def cinkeltkocka():
+    cinkelt_lista=[1, 2, 3, 4, 5, 6, 6, 6]
+    dobas:int=int(random.random()(cinkelt_lista)*20)
+    print(f"{dobas}")
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import feladatok
-lista = [12, 21, 56, 32, -5, -23, 35]
+#lista = [12, 21, 56, 32, -5, -23, 35]
 
-print("1. feladat")
-db:int=feladatok.poz_szamok_szama(lista)
-print(f"A pozitív számok száma: {db}")
+#print("1. feladat")
+#db:int=feladatok.poz_szamok_szama(lista)
+#print(f"A pozitív számok száma: {db}")
 
 
-print("2.feladat")
-szam:int=feladatok.negativ_szamok_osszege(lista)
-print(f"A negatív számok összege: {szam}")
+#print("2.feladat")
+#szam:int=feladatok.negativ_szamok_osszege(lista)
+#print(f"A negatív számok összege: {szam}")
 
-print("3. feladat")
-atlag:float=feladatok.otteloszthato(lista)
-print(f"Az öttel osztható számok átlaga: {atlag}")
+#print("3. feladat")
+#atlag:float=feladatok.otteloszthato(lista)
+#print(f"Az öttel osztható számok átlaga: {atlag}")
+
+#print("4. feladat")
+#eredmenyek_lista=feladatok.eremdobas()
+#print(eredmenyek_lista)
+
+#print("5. feladat")
+#fej:int=feladatok.fej_dobasok(eredmenyek_lista)
+#print(f"{eredmenyek_lista}")
+#print(f"A fej dobások száma: {fej}")
+
+#print("6. feladat")
+#kockadobasok, dobas=feladatok.kockadobas()
+#print(kockadobasok)
+
+#print("7. feladat")
+#egyes, kettes, harmas, negyes, otos, hatos=feladatok.kockadobas_szamolo(kockadobasok, dobas)
+#print(f"Egyes dobások száma: {egyes}, kettes dobsok száma: {kettes}, hármas dobások száma: {harmas}, négyes dobások száma: {negyes}, ötös dobások száma: {otos}, hatos dobások száma: {hatos}")
+
+feladatok.cinkeltkocka()
